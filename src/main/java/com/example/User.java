cc7a61d0483e46eba87b0ab7226fb6d06f0bfde7
RevolutionNeo <824047065@qq.com>
2022-07-28 20:07:22 +0800

    陈泰源第一版 1岁

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -3,7 +3,8 @@ package com.example;
 public class User {
     private int id;
     private String name;
-    private int age = 1;
+    private int age0 = 0;
+    private int age1 = 1;
     private String sex;
 
     public User() {
